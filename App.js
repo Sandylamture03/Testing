84346a551f9f5590b6d70247da8a47e5de540dbc
Sandy_Lamture <sandylamture03@gmail.com>
2025-01-31 16:41:48 +0530

    2nd comment

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an H1 Tag"),
-    React.createElement("h2", {}, "I am an H2 Tag"),
+    React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am an H2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an H1 Tag"),
-    React.createElement("h2", {}, "I am an H2 Tag"),
+    React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am an H2 tag"),
   ]),
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(parent);
